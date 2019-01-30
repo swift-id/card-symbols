1467ff56c3d934b830b882bb7ea97ad37527b536
R. Kukuh <rkukuh@gmail.com>
2019-01-30 09:17:19 +0700

    Finish task

--- Card Symbols/ViewController.swift
+++ Card Symbols/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardSymbol: UILabel!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if sender.tag == 1 {
+            cardSymbol.text = "♦"
+            cardSymbol.textColor = UIColor.red
+        }
+        
+        if sender.tag == 2 {
+            cardSymbol.text = "♠"
+            cardSymbol.textColor = UIColor.black
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
